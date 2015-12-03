f620b769fa0353271560b707d021573540d0de71
笑你妹c <ytx0573@sina.com>
2015-12-03 14:42:12 +0800

    生成日志方法

---
+++ Mac/Mac/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Mac
+//
+//  Created by Johnson on 15/12/2.
+//  Copyright © 2015年 Johnson. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
