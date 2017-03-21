6f28ed37f12aeecf9fcdf0d34d5bc752356cfacb
笑你妹c <ytx0573@gmail.com>
2017-03-21 15:41:44 +0800

    添加腾讯视频广告屏蔽

---
+++ johnson_qqlive/johnson_qqlive.h
@@ -0,0 +1,22 @@
+//
+//  xx.h
+//  MobileMail
+//
+//  Created by Johnson on 8/11/15.
+//  Copyright (c) 2015 Johnson. All rights reserved.
+//
+
+
+
+
+@interface TADQQViewController : UIViewController
+
+- (void)setDelegate:(id)arg;
+
+@end
+
+@interface KKMediaViewController : UIViewController
+
+- (void)skipAdPlay;
+
+@end
